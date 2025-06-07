768845475c8ffa633934b2bc658b282bfb140889
Ayush231104 <ayushzalavadiya23112004@gmail.com>
2025-06-07 10:51:10 +0530

    Base_url change2

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -1,10 +1,9 @@
 import axios from "axios";
 
 // export const BASE_URL = "https://prolinker-qs8c.onrender.com/";
-
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 
 export const clientServer = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
-  // baseURL: BASE_URL,
+  baseURL: BASE_URL,
 });
