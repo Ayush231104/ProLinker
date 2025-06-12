f775eea053762c9553ef5a180cdb138df13a6ef6
Ayush231104 <ayushzalavadiya23112004@gmail.com>
2025-06-12 13:58:07 +0530

    final Commit whith backend

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// export const BASE_URL = "https://prolinker-qs8c.onrender.com/";
-export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+export const BASE_URL = "https://prolinker-qs8c.onrender.com/";
+// export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 
 export const clientServer = axios.create({
